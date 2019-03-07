e6dfcdc1850e2318b97b8f0bf6d5470ad85c82ca
Ishendra <36564648+Ishendra@users.noreply.github.com>
2019-03-07 14:14:16 +0530

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,12 +4,12 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 var config = {
-    apiKey: "AIzaSyA3-ObYYDK2LloI-MM4wD7IBIX7l6Ek5Po",
-    authDomain: "bhole-1214d.firebaseapp.com",
-    databaseURL: "https://bhole-1214d.firebaseio.com",
-    projectId: "bhole-1214d",
-    storageBucket: "bhole-1214d.appspot.com",
-    messagingSenderId: '235280288937'
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ''
   };
   firebase.initializeApp(config);
 // Retrieve an instance of Firebase Messaging so that it can handle background
